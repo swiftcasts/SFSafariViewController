2b6704f72b5699a8b8953aee150e468d650360a8
Josh Bruce <josh@surftrack.co.uk>
2015-09-29 15:09:40 +0100

    Initial Commit

---
+++ SafariViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SafariViewController
+//
+//  Created by Josh Bruce on 29/09/2015.
+//  Copyright © 2015 Swiftcasts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
